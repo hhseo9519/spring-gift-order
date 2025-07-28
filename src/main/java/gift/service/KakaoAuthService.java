833433dfa40abd324ee069c39c01f97f5b1c90df
hhseo9519 <hhseo9519@naver.com>
2025-07-28 14:12:21 +0900

    feat : 타임아웃 설정 및 문자열 직접 작성하지 않고 객체로 관리

--- src/main/java/gift/service/KakaoAuthService.java
+++ src/main/java/gift/service/KakaoAuthService.java
@@ -1,11 +1,13 @@
 package gift.service;
 
 import gift.config.KakaoProperties;
+import gift.dto.KakaoTokenRequestDto;
 import gift.dto.KakaoTokenResponseDto;
 import gift.dto.KakaoUserResponseDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -14,11 +16,18 @@ public class KakaoAuthService {
 
     private final MemberService memberService;
     private final KakaoProperties kakaoProperties;
-    private final RestClient restClient = RestClient.create();
+    private final RestClient restClient;
 
     public KakaoAuthService(MemberService memberService, KakaoProperties kakaoProperties) {
         this.memberService = memberService;
         this.kakaoProperties = kakaoProperties;
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+
+        this.restClient = RestClient.builder()
+                .requestFactory(factory)
+                .build();
     }
 
     public KakaoTokenResponseDto requestAccessToken(String code) {
@@ -26,16 +35,16 @@ public class KakaoAuthService {
         String redirectUri = kakaoProperties.getRegistration().getKakao().getRedirectUri();
         String tokenUri = kakaoProperties.getProvider().getKakao().getTokenUri();
 
+        KakaoTokenRequestDto requestDto = new KakaoTokenRequestDto(clientId, redirectUri, code);
+
         return restClient.post()
                 .uri(tokenUri)
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body("grant_type=authorization_code" +
-                        "&client_id=" + clientId +
-                        "&redirect_uri=" + redirectUri +
-                        "&code=" + code)
+                .body(requestDto.FormingData())
                 .retrieve()
                 .body(KakaoTokenResponseDto.class);
     }
+
 
     private KakaoUserResponseDto requestUserInfo(String accessToken) {
         String userInfoUri = kakaoProperties.getProvider().getKakao().getUserInfoUri();
